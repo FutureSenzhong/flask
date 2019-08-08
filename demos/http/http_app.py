4f606bdf1526ff176cdd09b0ea0c2eecc20f3a71
sz <dengsz@ik.com>
2019-08-08 19:59:36 +0800

    监听请求方式

--- demos/http/http_app.py
+++ demos/http/http_app.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     name = request.args.get('name', 'Flask')  # 获取查询参数name的值
     return '<h1>Hello, %s!<h1>' % name
