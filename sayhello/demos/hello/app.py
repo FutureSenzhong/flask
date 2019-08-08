e63c20873773e1d9d24c526c835abc411f47bfe5
sz <dengsz@ik.com>
2019-08-08 17:58:11 +0800

    first commit,添加hello.py

---
+++ sayhello/demos/hello/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = 'dasdfjikkjhkjhkj'
+
+
+@app.route('/')
+def hello():
+    return '</h1>hello world</h1>'
+
+
+@app.route('/hi')
+@app.route('/hello')
+def say_hello():
+    return '<h1>Hello, Flask!</h1>'
+
+
+if __name__ == '__main__':
+    app.run()
