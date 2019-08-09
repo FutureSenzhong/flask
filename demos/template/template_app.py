2893d27edb4e8ed20c3fb682fc4864f92e1e05f4
sz <dengsz@ik.com>
2019-08-09 15:40:22 +0800

    设置模板空白控制的环境变量

--- demos/template/template_app.py
+++ demos/template/template_app.py
@@ -6,6 +6,12 @@ app = Flask(__name__, template_folder='./templates')
 app.secret_key = os.getenv('SECRET_KEY', 'dasdadadsasd')
 
 
+# 删除Jinja2语句后的第一个空行
+app.jinja_env.trim_blocks = True
+# 删除Jinja2语句所在行之前的空格和制表符（tabs）
+app.jinja_env.lstrip_blocks = True
+
+
 @app.route('/')
 def index():
     # 访问跟目录默认重定向到watchlist来显示数据
