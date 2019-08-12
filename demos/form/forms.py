f3150d6de5175de2ca946791968e305f6ed273e5
sz <dengsz@ik.com>
2019-08-12 18:25:26 +0800

    全局验证器(自定义验证器有参数和没有参数的两种写法)

--- demos/form/forms.py
+++ demos/form/forms.py
@@ -33,3 +33,30 @@ class FortyTwoForm(FlaskForm):
     def validate_answer(cls, field):
         if field.data != 42:
             raise ValidationError('Must be 42.')
+
+
+# 自定义验证器（全局验证器）没有参数的全局函数验证器
+# def is_42(form, field):
+#     if field.data != 42:
+#         raise ValidationError('Must be 42')
+#
+#
+# class FortyTwoForm(FlaskForm):
+#     answer = IntegerField('The Number', validators=[is_42])
+#     submit = SubmitField()
+
+
+# 自定义验证器（全局验证器）有参数的全局函数验证器（工厂函数）
+def is_42(message=None):
+    if message is None:
+        message = 'Must be 42.'
+
+    def _is_42(form, field):
+        if field.data != 42:
+            raise ValidationError(message)
+    return _is_42
+
+
+class FortyTwoForm1(FlaskForm):
+    answer = IntegerField('The Number', validators=[is_42()])
+    submit = SubmitField()
