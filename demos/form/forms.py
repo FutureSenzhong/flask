ad7c8040b388df1807b1edc8fc6d5df4eca4965c
sz <dengsz@ik.com>
2019-08-12 18:26:39 +0800

    全局验证器(类验证器)

--- demos/form/forms.py
+++ demos/form/forms.py
@@ -60,3 +60,19 @@ def is_42(message=None):
 class FortyTwoForm1(FlaskForm):
     answer = IntegerField('The Number', validators=[is_42()])
     submit = SubmitField()
+
+
+# 类验证器
+# class Length(object):
+#     def __init__(self, min=-1, max=-1, message=None):
+#         self.min = min
+#         self.max = max
+#         if not message:
+#             message = u'Field must be between %i and %i characters long.' % (min, max)
+#         self.message = message
+#
+#     def __call__(self, form, field):
+#         l = field.data and len(field.data) or 0
+#         if l < self.min or self.max != -1 and l > self.max:
+#             raise ValidationError(self.message)
+# length = Length
